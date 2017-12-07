cd68b736225a74414c22e101583baed2554123ed
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2017-12-07 16:36:00 +0400

    Move FLTK Window rendering to main.cpp

--- Makefile
+++ Makefile
@@ -1,4 +1,2 @@
 default:
-	g++ -std=c++11 `fltk-config --cxxflags` fltk.cpp  `fltk-config --ldflags` -lfltk -lfltk_images -ljpeg -o fltk
-boost:
-	g++ -I boost_1_65_1 main.cpp -o test
+	g++ -std=c++11 `fltk-config --cxxflags` main.cpp  `fltk-config --ldflags` -lfltk -lfltk_images -ljpeg -I ../boost_1_65_1 -o graph-toolbox
--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@
 #include "boost/graph/graph_traits.hpp"
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/graphviz.hpp"
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/fl_draw.H>
+#include <FL/Fl_Image.H>
+#include <FL/Fl_JPEG_Image.H>
 
 using namespace std;
 using namespace boost;
 
 const char* name[] = { "0", "1", "2", "3", "4" };
 
-int main()
+int main(int argc, char *argv[])
 {
   ofstream fout ("graph1.gv");
   int num_nodes, n;
@@ -36,4 +42,19 @@ int main()
   }
   write_graphviz(fout, g, make_label_writer(name));
   system("dot -Tjpeg graph1.gv -o image.jpg");
+
+  Fl_Window *window = new Fl_Window(1024, 640, "Graph Toolbox");
+
+  Fl_Box *title = new Fl_Box(360, 20, 300, 100, "Graph Toolbox");
+  title->box(FL_UP_BOX);
+  title->labelfont(FL_BOLD+FL_ITALIC);
+  title->labelsize(16);
+  
+  Fl_JPEG_Image img("image.jpg");
+  Fl_Box box(100, 50,img.w(),img.h());
+  box.image(img);
+  
+  window->end();
+  window->show(argc, argv);
+  return Fl::run();
 }
